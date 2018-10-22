6873be910b9a130825423ac1598aa46ccb7374a9
Maros Pekarik <maros@sld.gs>
2018-10-22 10:44:27 +0200

    misc change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit Change
+            New Change
             <code>src/App.js</code> and save to reload.
           </p>
           <a
