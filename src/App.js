151c47f49857a172b57c409310c6e3a3ced787e0
Maros Pekarik <maros@sld.gs>
2018-10-25 16:22:27 +0200

    humble test 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Testing app again!
+            Humble test
             <code>src/App.js</code> build now!
           </p>
           <a
